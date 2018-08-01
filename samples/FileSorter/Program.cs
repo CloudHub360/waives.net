dba50b4ef1edab57b10a7ccb426ba5f011c5df04
Alastair Smith <github@alastairsmith.me.uk>
2018-08-01 16:04:10 +0100

    Add docopt options parsing and documentation
    Describe the behaviour of the sample app, as well as the usage and
    options available. Ensure all directories specified exist before
    starting processing.

--- samples/FileSorter/Program.cs
+++ samples/FileSorter/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FileSorter
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private const string Usage = @"File system sorter sample app.
+
+    This sample application is supplied with the Waives.NET SDK to illustrate how to integrate
+    with Waives' classification functionality. It watches the specified directory, classifying
+    each file in that directory (including new ones added whilst the app is running) using the
+    specified classifier, and moving the files to a subdirectory within the output directory.
+    The subdirectory is named for the type of the document Waives identifies in the file.
+
+    Subdirectories of the watched directory are not processed. Documents which cannot be
+    processed will be written to the specified failures directory, or by default to a directory
+    named ""failures"" within the input directory. A log message is written to the failures
+    directory with the file indicating the error encountered in processing that file.
+
+    Usage:
+      FileSorter.exe watch <directory> using <classifier> (-o <outdir> | --output=<outdir>) [-f <faildir> | --failures=<faildir>]
+      FileSorter.exe (-h | --help)
+      FileSorter.exe --version
+
+    Options:
+      -h --help                           Show this screen.
+      -o <outdir>, --output=<outdir>      The directory into which files will be sorted.
+      -f <faildir>, --failures=<faildir>  The directory into which failed documents will be moved.
+
+    ";
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new DocoptNet.Docopt().Apply(Usage, args, version: "File system sorter sample app 1.0", exit: true);
+
+            var inbox = options["<directory>"].ToString();
+            var outbox = options["--output"].ToString();
+            var errorbox = options["--failures"]?.ToString()
+                           ?? Path.Combine(options["<directory>"].ToString(), "failures");
+
+            EnsureDirectoryExists(inbox);
+            EnsureDirectoryExists(outbox);
+            EnsureDirectoryExists(errorbox);
+
+            // Initialise and start here...
+
+            // Block until manually exited with Ctrl+C, console window closed, etc.
+            await Task.Run(() => Task.Delay(Timeout.Infinite));
+        }
+
+        private static void EnsureDirectoryExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
         }
     }
 }
