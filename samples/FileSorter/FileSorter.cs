7e05ebc210447f4fdc8a205c775f5eb2672743c7
Alastair Smith <github@alastairsmith.me.uk>
2018-08-03 18:17:26 +0100

    Add the file sorting logic
    With the pieces all in place now, we can start to wire up the file
    sorter itself. When a classification result is received, move the file
    to a subfolder of the outbox named for the document's classification.

---
+++ samples/FileSorter/FileSorter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Waives.NET;
+
+namespace FileSorter
+{
+    public class FileSorter : IObserver<DocumentClassification>
+    {
+        private readonly string _outboxPath;
+        private readonly string _errorboxPath;
+
+        public FileSorter(string outboxPath, string errorboxPath)
+        {
+            _outboxPath = outboxPath;
+            _errorboxPath = errorboxPath;
+        }
+
+        public void OnCompleted()
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {
+            // Move to failures box
+        }
+
+        public void OnNext(DocumentClassification value)
+        {
+            if (!(value.Document is FileSystemDocumentSource documentSource))
+            {
+                throw new InvalidOperationException("Cannot move");
+            }
+
+            MoveFile(documentSource, _outboxPath, value.ClassificationResult.DocumentType);
+        }
+
+        private void MoveFile(FileSystemDocumentSource fileSystemDocumentSource, string boxPath, string subfolder = "")
+        {
+            var destination = Path.Combine(boxPath, subfolder);
+            EnsureDirectoryExists(destination);
+
+            try
+            {
+                File.Move(fileSystemDocumentSource.FilePath.FullName,
+                    Path.Combine(destination, fileSystemDocumentSource.FilePath.Name));
+            }
+            catch (IOException ex)
+            {
+                OnError(ex);
+            }
+        }
+
+        private static void EnsureDirectoryExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
