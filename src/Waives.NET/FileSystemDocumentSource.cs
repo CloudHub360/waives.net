441616c5f984baa2e3ddad70bb19fba9fc3afc11
Alastair Smith <github@alastairsmith.me.uk>
2018-08-03 17:57:00 +0100

    Define an IDocumentSource and filesystem impl
    Provide an abstraction for reading documents prior to processing.
    Initially we only need a filesystem-based implementation, but others
    might include a database, a cloud storage account, etc.

---
+++ src/Waives.NET/FileSystemDocumentSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Waives.NET
+{
+    public class FileSystemDocumentSource : IDocumentSource
+    {
+        private readonly string _filePath;
+
+        public FileInfo FilePath => new FileInfo(_filePath);
+
+        public FileSystemDocumentSource(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            _filePath = filePath;
+        }
+
+        public Task<Stream> OpenStream()
+        {
+            return Task.FromResult(File.OpenRead(_filePath) as Stream);
+        }
+    }
+}
---
+++ src/Waives.NET/IDocumentSource.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Waives.NET
+{
+    public interface IDocumentSource
+    {
+        Task<Stream> OpenStream();
+    }
+}
