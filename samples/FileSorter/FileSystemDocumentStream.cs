75385d6c1a57877da7e754c2ffa2467eaad60e7c
Alastair Smith <github@alastairsmith.me.uk>
2018-08-03 18:03:20 +0100

    Add a DocumentStream abstraction
    This abstraction represents a stream of documents to be processed, with
    an implementation provided by the FileSorter sample application to
    funnel files created in the watched inbox into the DocumentStream.
    
    The sample implementation illustrates how to convert an event stream
    into an IObservable, and how to convert an IEnumerable to an
    IObservable.

---
+++ samples/FileSorter/FileSystemDocumentStream.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+using Waives.NET;
+
+namespace FileSorter
+{
+    public class FileSystemDocumentStream : DocumentStream
+    {
+        private FileSystemDocumentStream(IObservable<IDocumentSource> buffer) : base(buffer)
+        {
+        }
+
+        public static DocumentStream Create(string inbox)
+        {
+            var watcher = new FileSystemWatcher(inbox)
+            {
+                IncludeSubdirectories = false,
+                EnableRaisingEvents = true
+            };
+
+            var inboxObservable = Observable
+                .FromEventPattern<FileSystemEventArgs>(watcher, nameof(watcher.Created))
+                .Select(e => new FileSystemDocumentSource(e.EventArgs.FullPath));
+
+            var initialContentsObservable = Directory
+                .EnumerateFiles(inbox).ToObservable()
+                .Select(p => new FileSystemDocumentSource(p));
+
+            return new FileSystemDocumentStream(inboxObservable.Merge(initialContentsObservable));
+        }
+    }
+}
---
+++ src/Waives.NET/DocumentStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace Waives.NET
+{
+    public abstract class DocumentStream : IObservable<IDocumentSource>
+    {
+        private readonly IObservable<IDocumentSource> _observable;
+
+        protected DocumentStream(IObservable<IDocumentSource> buffer)
+        {
+            _observable = buffer;
+        }
+
+        public IDisposable Subscribe(IObserver<IDocumentSource> observer)
+        {
+            _observable.Subscribe(observer);
+            return Disposable.Empty;
+        }
+    }
+}
