ef98b073348e26de93fdda172316f3074ac27490
Alastair Smith <github@alastairsmith.me.uk>
2018-08-03 18:11:33 +0100

    Add a ClassificationResultStream
    Similar to the DocumentStream, this defines a stream of classification
    results. Classification results can only be obtained from documents, so
    it depends on a document stream as its source. It pushes the document
    along with its classification results to its observers.

---
+++ src/Waives.NET/ClassificationResultStream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Waives.Client;
+
+namespace Waives.NET
+{
+    public class ClassificationResultStream : IObservable<DocumentClassification>
+    {
+        private readonly Classifier _classifier;
+        private readonly IObservable<IDocumentSource> _documentStream;
+
+        public ClassificationResultStream(Classifier classifier, IObservable<IDocumentSource> documentStream)
+        {
+            _classifier = classifier;
+            _documentStream = documentStream;
+        }
+
+        public IDisposable Subscribe(IObserver<DocumentClassification> observer)
+        {
+            var subscription = _documentStream.Select(d => Task.Run(() => ClassifyDocument(d)).Result).Subscribe(observer);
+            return Disposable.Create(() => subscription.Dispose());
+        }
+
+        private async Task<DocumentClassification> ClassifyDocument(IDocumentSource document)
+        {
+            using (var documentStream = await document.OpenStream().ConfigureAwait(false))
+            {
+                var classificationResult = await _classifier.Classify(documentStream).ConfigureAwait(false);
+                return new DocumentClassification(document, classificationResult);
+            }
+        }
+    }
+}
---
+++ src/Waives.NET/DocumentClassification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Waives.Client.Responses;
+
+namespace Waives.NET
+{
+    public class DocumentClassification
+    {
+        public DocumentClassification(IDocumentSource document, ClassificationResult classificationResult)
+        {
+            Document = document ?? throw new ArgumentNullException(nameof(document));
+            ClassificationResult = classificationResult ?? throw new ArgumentNullException(nameof(classificationResult));
+        }
+
+        public IDocumentSource Document { get; }
+
+        public ClassificationResult ClassificationResult { get; }
+    }
+}
