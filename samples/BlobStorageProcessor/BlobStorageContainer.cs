ba8b327122e3ed8549848150d4e162e0a9b2eb81
Alastair Smith <github@alastairsmith.me.uk>
2018-08-07 11:11:47 +0100

    Extract class for Blob Storage operations
    Move blob enumeration for a container to a new class.

---
+++ samples/BlobStorageProcessor/BlobStorageContainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobStorageProcessor
+{
+    internal class BlobStorageContainer
+    {
+        private readonly CloudBlobContainer _containerReference;
+
+        public BlobStorageContainer(CloudBlobContainer containerReference)
+        {
+            _containerReference = containerReference ?? throw new ArgumentNullException(nameof(containerReference));
+        }
+
+        public async Task<IEnumerable<CloudBlockBlob>> GetBlobs()
+        {
+            BlobContinuationToken continuation = null;
+            var allBlobs = new List<CloudBlockBlob>();
+
+            do
+            {
+                var segment = await _containerReference.ListBlobsSegmentedAsync(continuation);
+                continuation = segment.ContinuationToken;
+
+                var directories = segment.Results.OfType<CloudBlobDirectory>();
+                var blobsInDirectories = (await Task.WhenAll(directories.Select(GetBlobsInDirectory))).SelectMany(d => d);
+                allBlobs.AddRange(segment.Results.OfType<CloudBlockBlob>().Concat(blobsInDirectories));
+            } while (continuation != null);
+
+            return allBlobs;
+        }
+
+        private static async Task<IEnumerable<CloudBlockBlob>> GetBlobsInDirectory(CloudBlobDirectory directory)
+        {
+            BlobContinuationToken continuation = null;
+            var allBlobs = new List<CloudBlockBlob>();
+
+            do
+            {
+                var blobSegment = await directory.ListBlobsSegmentedAsync(continuation);
+                continuation = blobSegment.ContinuationToken;
+
+                var directories = blobSegment.Results.OfType<CloudBlobDirectory>();
+                var subDirectoryBlobs = (await Task.WhenAll(directories.Select(GetBlobsInDirectory))).SelectMany(d => d);
+
+                allBlobs.AddRange(blobSegment.Results.OfType<CloudBlockBlob>().Concat(subDirectoryBlobs));
+            } while (continuation != null);
+
+            return allBlobs;
+        }
+    }
+}
--- samples/BlobStorageProcessor/Program.cs
+++ samples/BlobStorageProcessor/Program.cs
@@ -47,23 +47,23 @@ namespace BlobStorageProcessor
             // Point at a blob storage account
             var options = new Docopt().Apply(Usage, args, version: "Blob Storage processor sample app 1.0", exit: true);
 
-            IEnumerable<CloudBlobContainer> containers;
+            IEnumerable<BlobStorageContainer> containers;
             if (options["--connection-string"] != null)
             {
                 var connectionString = options["--connection-string"].ToString();
                 var containerNames = options["<container>"].AsEnumerable(i => i.ToString());
 
                 var client = CloudStorageAccount.Parse(connectionString).CreateCloudBlobClient();
-                containers = containerNames.Select(n => client.GetContainerReference(n));
+                containers = containerNames.Select(n => new BlobStorageContainer(client.GetContainerReference(n)));
             }
             else
             {
                 var containerSas = options["--container-sas"].ToString();
-                containers = new[] { new CloudBlobContainer(new Uri(containerSas)) };
+                containers = new[] { new BlobStorageContainer(new CloudBlobContainer(new Uri(containerSas))) };
             }
 
             // Identify all blobs there
-            var blobs = (await Task.WhenAll(containers.Select(GetBlobsInContainer))).SelectMany(c => c);
+            var blobs = (await Task.WhenAll(containers.Select(c => c.GetBlobs()))).SelectMany(c => c);
 
             // Log in to Waives
             var waives = new WaivesClient();
@@ -77,43 +77,6 @@ namespace BlobStorageProcessor
             classificationResults.SubscribeConsole("classification");
 
             // Write results to a CSV file
-        }
-
-        private static async Task<IEnumerable<CloudBlockBlob>> GetBlobsInContainer(CloudBlobContainer container)
-        {
-            BlobContinuationToken continuation = null;
-            var allBlobs = new List<CloudBlockBlob>();
-
-            do
-            {
-                var segment = await container.ListBlobsSegmentedAsync(continuation);
-                continuation = segment.ContinuationToken;
-
-                var directories = segment.Results.OfType<CloudBlobDirectory>();
-                var blobsInDirectories = (await Task.WhenAll(directories.Select(GetBlobsInDirectory))).SelectMany(d => d);
-                allBlobs.AddRange(segment.Results.OfType<CloudBlockBlob>().Concat(blobsInDirectories));
-            } while (continuation != null);
-
-            return allBlobs;
-        }
-
-        private static async Task<IEnumerable<CloudBlockBlob>> GetBlobsInDirectory(CloudBlobDirectory directory)
-        {
-            BlobContinuationToken continuation = null;
-            var allBlobs = new List<CloudBlockBlob>();
-
-            do
-            {
-                var blobSegment = await directory.ListBlobsSegmentedAsync(continuation);
-                continuation = blobSegment.ContinuationToken;
-
-                var directories = blobSegment.Results.OfType<CloudBlobDirectory>();
-                var subDirectoryBlobs = (await Task.WhenAll(directories.Select(GetBlobsInDirectory))).SelectMany(d => d);
-
-                allBlobs.AddRange(blobSegment.Results.OfType<CloudBlockBlob>().Concat(subDirectoryBlobs));
-            } while (continuation != null);
-
-            return allBlobs;
         }
     }
 }
