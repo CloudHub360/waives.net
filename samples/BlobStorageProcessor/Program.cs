10280b2d51c3b0b27b9d4cf6b8dddd847ae4c7f9
Alastair Smith <github@alastairsmith.me.uk>
2018-08-07 09:55:47 +0100

    Read all blob references from specified containers
    Parse arguments into blob container references. For SAS connections, we
    create a singleton array with the container reference for the given SAS.
    For connection strings, we take the supplied container names and obtain
    container references for each of them.
    
    With the container references determined, we can then retrieve all the
    blobs in those containers.

--- samples/BlobStorageProcessor/Program.cs
+++ samples/BlobStorageProcessor/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DocoptNet;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace BlobStorageProcessor
 {
@@ -41,11 +45,45 @@ namespace BlobStorageProcessor
             // Point at a blob storage account
             var options = new Docopt().Apply(Usage, args, version: "Blob Storage processor sample app 1.0", exit: true);
 
+            IEnumerable<CloudBlobContainer> containers;
+            if (options["--connection-string"] != null)
+            {
+                var connectionString = options["--connection-string"].ToString();
+                var containerNames = options["<container>"].AsEnumerable(i => i.ToString());
+
+                var client = CloudStorageAccount.Parse(connectionString).CreateCloudBlobClient();
+                containers = containerNames.Select(n => client.GetContainerReference(n));
+            }
+            else
+            {
+                var containerSas = options["--container-sas"].ToString();
+                containers = new[] { new CloudBlobContainer(new Uri(containerSas)) };
+            }
+
             // Identify all blobs there
+            var blobs = containers.SelectMany(GetBlobsInContainer).ToList();
+            Console.WriteLine($"Found {blobs.Count} blobs in containers {string.Join(", ", containers.Select(c => c.Name))}");
+
             // Classify the files
             // Write results to a CSV file
+        }
 
+        private static IEnumerable<CloudBlockBlob> GetBlobsInContainer(CloudBlobContainer container)
+        {
+            var continuation = new BlobContinuationToken();
+            var allBlobs = Task.Run(() => container.ListBlobsSegmentedAsync(continuation)).Result.Results;
+            var blobsInDirectories = allBlobs.OfType<CloudBlobDirectory>().SelectMany(GetBlobsInDirectory);
+            var blobs = allBlobs.OfType<CloudBlockBlob>().Concat(blobsInDirectories);
+            return blobs;
+        }
 
+        private static IEnumerable<CloudBlockBlob> GetBlobsInDirectory(CloudBlobDirectory directory)
+        {
+            var continuation = new BlobContinuationToken();
+            var allBlobs = Task.Run(() => directory.ListBlobsSegmentedAsync(continuation)).Result.Results;
+            var subDirectoryBlobs = allBlobs.OfType<CloudBlobDirectory>().SelectMany(GetBlobsInDirectory);
+
+            return allBlobs.OfType<CloudBlockBlob>().Concat(subDirectoryBlobs);
         }
     }
 }
