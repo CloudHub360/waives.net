158ced14c4de8885661bd93fa8cd7fc8736d8ca3
Alastair Smith <github@alastairsmith.me.uk>
2018-08-07 09:45:20 +0100

    Add AsEnumerable extension to Docopt
    This makes the Docopt interface LINQ-friendly for multi-value arguments.

---
+++ samples/BlobStorageProcessor/DocoptExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// ReSharper disable once CheckNamespace
+namespace DocoptNet
+{
+    public static class DocoptExtensions
+    {
+        public static IEnumerable<T> AsEnumerable<T>(this ValueObject option, Func<object, T> itemProjection)
+        {
+            var items = new List<T>();
+            foreach (var item in option.AsList)
+            {
+                items.Add(itemProjection(item));
+            }
+
+            return items;
+        }
+    }
+}
