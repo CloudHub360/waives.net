5777f8e9bdb1c626979a862c49547f6d8d6096a1
Alastair Smith <github@alastairsmith.me.uk>
2018-08-09 12:39:28 +0100

    Improve variable names

--- src/Waives.Extensions.DocumentChannels.Filesystem/FileSystemDocumentChannel.cs
+++ src/Waives.Extensions.DocumentChannels.Filesystem/FileSystemDocumentChannel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reactive.Linq;
 
 namespace Waives.Extensions.DocumentChannels.Filesystem
@@ -12,9 +13,10 @@ namespace Waives.Extensions.DocumentChannels.Filesystem
 
         public static DocumentChannel Create(string inbox, bool watch = false)
         {
-            var initialContentsObservable = Directory
-                .EnumerateFiles(inbox).ToObservable()
-                .Select(p => new FileSystemDocumentSource(p));
+            var initialContents = Directory
+                .EnumerateFiles(inbox)
+                .Select(p => new FileSystemDocumentSource(p))
+                .ToObservable();
 
             if (watch)
             {
@@ -24,14 +26,14 @@ namespace Waives.Extensions.DocumentChannels.Filesystem
                     EnableRaisingEvents = true
                 };
 
-                var inboxObservable = Observable
+                var createdFiles = Observable
                     .FromEventPattern<FileSystemEventArgs>(watcher, nameof(watcher.Created))
                     .Select(e => new FileSystemDocumentSource(e.EventArgs.FullPath));
 
-                return new FileSystemDocumentChannel(initialContentsObservable.Concat(inboxObservable));
+                return new FileSystemDocumentChannel(initialContents.Concat(createdFiles));
             }
 
-            return new FileSystemDocumentChannel(initialContentsObservable);
+            return new FileSystemDocumentChannel(initialContents);
         }
     }
 }
