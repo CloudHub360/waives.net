e8d9b73f8c81dfd956a0ac01cecf9f847b89ba2e
Alastair Smith <github@alastairsmith.me.uk>
2018-08-13 13:45:31 +0100

    Define the pipeline with a document source
    The first thing that a document processing pipeline needs is the source
    document for it to open and read the contents from.
    `WithDocumentsFrom()` defines that part of the pipeline: the document
    source.
    
    This contains a naive implementation of this processing step which opens
    the document and streams it to the Waives API for creation, wrapping up
    the source document and the object created in the Waives API within a
    WaivesDocument object. This object is the document which will be
    processed within the remainder of the pipeline.

--- src/Waives.Reactive/PipelineBuilder.cs
+++ src/Waives.Reactive/PipelineBuilder.cs
@@ -1,6 +1,85 @@
-﻿namespace Waives.Reactive
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace Waives.Reactive
 {
+    /// <summary>
+    /// Configures a new document-processing pipeline.
+    /// </summary>
+    /// <example>
+    /// <![CDATA[
+    /// using System;
+    /// using System.Threading.Tasks;
+    /// using Waives.Reactive;
+    /// using Waives.Reactive.Extensions.DocumentSources.Filesystem
+    ///
+    /// namespace Waives.Reactive.Example
+    /// {
+    ///     public class Program
+    ///     {
+    ///         public static void Main(string[] args)
+    ///         {
+    ///             Task.Run(() => MainAsync(args)).Wait();
+    ///         }
+    ///
+    ///         public static async Task MainAsync(string[] args)
+    ///         {
+    ///             await WaivesApi.Login("clientId", "clientSecret");
+    ///             var pipeline = WaivesApi.CreatePipeline();
+    ///
+    ///             pipeline
+    ///                 .WithDocumentsFrom(FileSystemSource.Create(@"C:\temp\inbox"))
+    ///                 .ClassifyWith("mortgages")
+    ///                 .Then(d => Console.WriteLine(d.ClassificationResults.DocumentType))
+    ///                 .OnPipelineCompeleted(_ => Console.WriteLine("Processing complete!"));
+    ///
+    ///             try
+    ///             {
+    ///                 pipeline.Start();
+    ///             }
+    ///             catch (WaivesException ex)
+    ///             {
+    ///                 Console.WriteLine($"Pipeline processing failed: {ex.InnerException.Message}");
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ]]>
+    /// </example>
     public class PipelineBuilder
     {
+        private IObservable<WaivesDocument> _pipeline = Observable.Empty<WaivesDocument>();
+
+        /// <summary>
+        /// Add the source of documents to the pipeline. This represents the start
+        /// of the pipeline
+        /// </summary>
+        /// <param name="documentSource"></param>
+        /// <returns>The modified <see cref="PipelineBuilder"/>.</returns>
+        public PipelineBuilder WithDocumentsFrom(IObservable<Document> documentSource)
+        {
+            _pipeline = documentSource.SelectMany(async d =>
+            {
+                using (var documentStream = await d.OpenStream().ConfigureAwait(false))
+                {
+                    return new WaivesDocument(d,
+                        await WaivesApi.ApiClient.CreateDocument(documentStream).ConfigureAwait(false));
+                }
+            });
+
+            return this;
+        }
+
+        /// <summary>
+        /// Start processing the documents in the pipeline.
+        /// </summary>
+        /// <returns>An <see cref="IDisposable"/> subscription object. If you wish to terminate
+        /// the document processing pipeline manually, invoke <see cref="IDisposable.Dispose"/> on
+        /// this object.</returns>
+        public IDisposable Start()
+        {
+            return Disposable.Empty;
+        }
     }
 }
---
+++ src/Waives.Reactive/WaivesDocument.cs
@@ -0,0 +1,18 @@
+﻿namespace Waives.Reactive
+{
+    /// <summary>
+    /// Represents a document within the Waives API.
+    /// </summary>
+    public class WaivesDocument
+    {
+        private readonly Http.Document _waivesDocument;
+
+        internal WaivesDocument(Document source, Http.Document waivesDocument)
+        {
+            Source = source;
+            _waivesDocument = waivesDocument;
+        }
+
+        public Document Source { get; }
+    }
+}
