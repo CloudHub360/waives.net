e3cee47f0b8a866af4be358e3fdca91c3e42bdfd
Alastair Smith <github@alastairsmith.me.uk>
2018-08-13 17:57:39 +0100

    Add Adapter classes for wrapping Waives' HTTP API
    Wrapping only the WaivesClient has proven not to be sufficient: we also
    need to wrap the individual document operations, exposed on
    Http.Document.

---
+++ src/Waives.Reactive/HttpAdapters/HttpDocument.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Waives.Http.Responses;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+namespace Waives.Reactive.HttpAdapters
+{
+    /// <summary>
+    /// Adapter interface to allow us to substitute the underlying calls.
+    /// </summary>
+    internal interface IHttpDocument
+    {
+        Task<ClassificationResult> Classify(string classifierName);
+    }
+
+    /// <summary>
+    /// Adapter class to provide a live implementation of <see cref="IHttpDocument"/>
+    /// with <see cref="Waives.Http.Document"/>. Every method on this class should
+    /// simply delegate to the matching method on <see cref="Waives.Http.Document"/>,
+    /// it should not implement any additional logic.
+    /// </summary>
+    internal class HttpDocument : IHttpDocument
+    {
+        private readonly Http.Document _documentClient;
+
+        internal HttpDocument(Http.Document documentClient)
+        {
+            _documentClient = documentClient;
+        }
+
+        public async Task<ClassificationResult> Classify(string classifierName)
+        {
+            return await _documentClient.Classify(classifierName).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Waives.Reactive/HttpAdapters/WaivesDocumentFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Waives.Http;
+
+namespace Waives.Reactive.HttpAdapters
+{
+    /// <summary>
+    /// Adapter interface for creating documents.
+    /// </summary>
+    internal interface IHttpDocumentFactory
+    {
+        Task<IHttpDocument> CreateDocument(Document source);
+    }
+
+    /// <summary>
+    /// Default implementation of <see cref="IHttpDocumentFactory"/>, creating
+    /// documents in the Waives API.
+    /// </summary>
+    internal class HttpDocumentFactory : IHttpDocumentFactory
+    {
+        private readonly IWaivesClient _apiClient;
+
+        internal HttpDocumentFactory(IWaivesClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<IHttpDocument> CreateDocument(Document source)
+        {
+            using (var documentStream = await source.OpenStream().ConfigureAwait(false))
+            {
+                return new HttpDocument(await _apiClient.CreateDocument(documentStream).ConfigureAwait(false));
+            }
+        }
+    }
+}
