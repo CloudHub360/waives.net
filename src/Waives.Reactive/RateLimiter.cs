631e76129518bb594bbb075a9b2c5c8be479ee2f
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-14 09:44:42 +0100

    Add RateLimiter and first test
    Run the timer which checks for available slots on the test scheduler
    rather than the default scheduler. Schedule the freeing of the slot at 3
    seconds within the virtual timeline; i.e., after the initial batch of
    documents have completed, but before the observer is disposed.

---
+++ src/Waives.Reactive/RateLimiter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace Waives.Reactive
+{
+    //Inspired by http://granitestatehacker.kataire.com/2016/01/rate-limiting-observables-with-reactive.html
+    public class RateLimiter
+    {
+        private readonly IScheduler _scheduler;
+        private long _availableDocumentSlots;
+        public const byte MaximumConcurrentDocuments = 10;
+
+        public RateLimiter(IScheduler scheduler = null)
+        {
+            _scheduler = scheduler ?? Scheduler.Default;
+            _availableDocumentSlots = MaximumConcurrentDocuments;
+        }
+
+        public void MakeDocumentSlotAvailable()
+        {
+            if (Interlocked.Read(ref _availableDocumentSlots) < MaximumConcurrentDocuments)
+            {
+                Interlocked.Increment(ref _availableDocumentSlots);
+                Console.WriteLine($"RateLimiter: Freed slot. There are now {_availableDocumentSlots} available slots.");
+            }
+        }
+
+        /// <summary>
+        /// Transforms an observable sequence of documents into one where the documents are only
+        /// emitted to the observer when slots are available within the maximum concurrency limit.
+        /// </summary>
+        /// <param name="sourceDocuments">An observable sequence of documents to be rate limited</param>
+        /// <returns>An observable sequence of documents which is rate limited to maximum concurrency</returns>
+        public IObservable<TSource> RateLimited<TSource>(IObservable<TSource> source)
+        {
+            var timeSpan = TimeSpan.FromMilliseconds(500);
+            bool sourceCompleted = false;
+
+            return Observable.Create<TSource>(
+                observer =>
+                {
+                    var buffer = new ConcurrentQueue<TSource>();
+                    Action emitIfSlotAvailable = delegate ()
+                    {
+                        while (Interlocked.Read(ref _availableDocumentSlots) > 0)
+                        {
+                            TSource item;
+                            if (!buffer.TryDequeue(out item))
+                            {
+                                Console.WriteLine("RateLimiter: No more items on queue");
+
+                                if (sourceCompleted)
+                                {
+                                    Console.WriteLine("RateLimiter: Source has completed, so calling observer.OnCompleted");
+                                    observer.OnCompleted();
+                                }
+                                break;
+                            }
+
+                            Console.WriteLine($"RateLimiter: There are {_availableDocumentSlots} slots free. Emitting item {item.ToString()}");
+                            observer.OnNext(item);
+                            Interlocked.Decrement(ref _availableDocumentSlots);
+                        }
+                    };
+
+                    var sourceSub = source
+                        .Subscribe(x =>
+                        {
+                            Console.WriteLine($"RateLimiter: Enqueuing {x.ToString()}");
+                            buffer.Enqueue(x);
+                            emitIfSlotAvailable();
+                        }, () =>
+                        {
+                            Console.WriteLine("RateLimiter: Source has completed. RateLimiter will complete on next check where queue is empty.");
+                            sourceCompleted = true;
+                        });
+
+                    var timer = Observable.Interval(timeSpan, _scheduler)
+                        .Subscribe(x =>
+                        {
+                            Console.WriteLine("DebugRateLimiter: Running check...");
+                            emitIfSlotAvailable();
+                        }, observer.OnError, observer.OnCompleted);
+                    return new CompositeDisposable(sourceSub, timer);
+                });
+        }
+    }
+}
---
+++ test/Waives.Reactive.Tests/RateLimiterFacts.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Disposables;
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace Waives.Reactive.Tests
+{
+    public class RateLimiterFacts
+    {
+        [Fact]
+        public void Only_output_maximum_allowed_number_of_documents_when_there_are_more_in_input()
+        {
+            var scheduler = new TestScheduler();
+            var sut = new RateLimiter(scheduler);
+
+            // Schedule one more document than the maximum concurrency, all to be scheduled immediately
+            var source = scheduler
+                .CreateColdObservable(AnArrayOfDocumentNotifications(RateLimiter.MaximumConcurrentDocuments+1));
+
+            var rateLimitedDocuments = sut.RateLimited(source);
+
+            var testObserver = scheduler.Start(() => rateLimitedDocuments);
+
+            Assert.Equal(RateLimiter.MaximumConcurrentDocuments, testObserver.Messages.Count);
+
+            for (var index = 0; index < testObserver.Messages.Count; index++)
+            {
+                var expectedDocumentSourceId = index.ToString();
+                var actualDocumentSourceId = testObserver.Messages[index].Value.Value.SourceId;
+                Assert.Equal(expectedDocumentSourceId, actualDocumentSourceId);
+            }
+        }
+
+        [Fact]
+        public void Outputs_the_documents_as_slots_become_free()
+        {
+            var scheduler = new TestScheduler();
+            var sut = new RateLimiter(scheduler);
+
+            var source = scheduler
+                .CreateColdObservable(AnArrayOfDocumentNotifications(RateLimiter.MaximumConcurrentDocuments + 1));
+
+            scheduler.ScheduleAbsolute(sut, TimeSpan.FromSeconds(3).Ticks, (_, rateLimiter) =>
+            {
+                rateLimiter.MakeDocumentSlotAvailable();
+                return Disposable.Empty;
+            });
+
+            var testObserver = scheduler.Start(() => sut.RateLimited(source),
+                created: 0, subscribed: 0, disposed: TimeSpan.FromSeconds(5).Ticks);
+
+            Assert.Equal(11, testObserver.Messages.Count);
+        }
+
+        /// <summary>
+        /// Create an array of Document notifications, where each document is scheduled at 1 tick of the
+        /// virtual scheduler
+        /// </summary>
+        /// <param name="numberOfDocuments">The number of document notifications</param>
+        /// <returns></returns>
+        private static Recorded<Notification<Document>>[] AnArrayOfDocumentNotifications(long numberOfDocuments)
+        {
+            var notifications = new List<Recorded<Notification<Document>>>();
+            for (long i = 0; i < numberOfDocuments; i++)
+            {
+                notifications.Add(new Recorded<Notification<Document>>(0,
+                    Notification.CreateOnNext<Document>(new TestDocument(Generate.Bytes(), i.ToString()))));
+            }
+
+            return notifications.ToArray();
+        }
+    }
+}
