83ddbc1369f7415371b89883ab7fcdba8cc35397
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-15 23:15:41 +0100

    Add ObservableProcessExtension
    This processes a sequence of WaivesDocuments, running an action on each. If the action succeeds, the document is included in the return observable. If the action throws an exception, the error action is run with that document wrapped in a ProcessingError and the document is not included in the return observable.

---
+++ src/Waives.Pipelines/ProcessingError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Waives.Pipelines
+{
+    internal class ProcessingError
+    {
+        public WaivesDocument Document { get; }
+        public Exception Exception { get; }
+
+        public ProcessingError(WaivesDocument document, Exception exception)
+        {
+            Document = document;
+            Exception = exception;
+        }
+    }
+}
---
+++ src/Waives.Pipelines/ProcessingResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Waives.Pipelines
+{
+    internal class ProcessingResult
+    {
+        public WaivesDocument Document { get; }
+        public bool ProcessedSuccessfully { get; }
+
+        public ProcessingResult(WaivesDocument document, bool processedSuccessfully)
+        {
+            Document = document;
+            ProcessedSuccessfully = processedSuccessfully;
+        }
+    }
+}
