3e79136f65ff3b7f9ce997d5b1205ee3fa0f1e13
Alastair Smith <github@alastairsmith.me.uk>
2018-08-16 17:15:06 +0100

    Add Get All Documents as an HTTP client operation
    From a `WaivesClient` instance, you can now say `GetAllDocuments()` to
    allow you to, for example, delete all existing documents at the start of
    a processing step.

---
+++ src/Waives.Http/Responses/DocumentCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Waives.Http.Responses
+{
+    internal class DocumentCollection
+    {
+        [JsonProperty("documents")]
+        internal IEnumerable<HalResponse> Documents { get; set; }
+    }
+}
