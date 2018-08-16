f40051e4106c8aca1c3943904e151fcf7abdfaa3
Alastair Smith <github@alastairsmith.me.uk>
2018-08-16 17:55:30 +0100

    Add a general-purpose EnumerableDocumentSource
    This takes an `IEnumerable<Document>` and converts it to an
    `IObservable<Document>` under the covers. It should make starting a
    Waives pipeline a bit easier.

---
+++ src/Waives.Reactive/EnumerableDocumentSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace Waives.Reactive
+{
+    /// <summary>
+    /// Adapter class converting an <see cref="IEnumerable{Document}"/> into a
+    /// <see cref="DocumentSource"/> for use as the starting point of a <see cref="Pipeline"/>.
+    /// </summary>
+    public class EnumerableDocumentSource : DocumentSource
+    {
+        public EnumerableDocumentSource(IEnumerable<Document> source) : base(source.ToObservable())
+        {
+        }
+    }
+}
