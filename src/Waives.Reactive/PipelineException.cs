af5867de4998208bd588b525b5d4898526c54686
Alastair Smith <github@alastairsmith.me.uk>
2018-08-16 10:55:59 +0100

    Add a PipelineException for fatal errors
    Some processing errors are recoverable (e.g. if creating a document
    fails, skip and move on to the next), but some are not (e.g.
    authentication failed, so nothing will work). In the case of the
    unrecoverable errors, we need to surface this to the calling application
    by way of a PipelineException.

---
+++ src/Waives.Reactive/PipelineException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Waives.Reactive
+{
+    /// <summary>
+    /// Indicates an unrecoverable error occurred in the processing of the pipeline.
+    /// </summary>
+    public class PipelineException : Exception
+    {
+        public PipelineException(Exception innerException)
+            : base("A fatal error occurred in the processing pipeline. View the InnerException" +
+                   "for more details.", innerException)
+        {
+
+        }
+    }
+}
