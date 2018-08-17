4f98edfc06670d8af0e81a25cb6a13147755ff05
Alastair Smith <github@alastairsmith.me.uk>
2018-08-17 14:38:45 +0100

    Add EventingDocumentSource and DocumentEmitter
    This provides an easier API for event-based document sources such as the
    FileSystemDocumentSource currently implemented by the file system
    extension and used by the File Sorter sample app. It can be used to
    implement either a pure event-based source, or a source requiring a
    worker thread, such as running a query against a database on a regular
    basis. It supports cancellation of long-running worker threads via the
    standard CancellationToken object.

---
+++ src/Waives.Reactive/DocumentEmitter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Waives.Reactive
+{
+    /// <summary>
+    /// Wraps an event stream (logical or literal) producing documents
+    /// and provides its own <see cref="NewDocument"/> event emitting
+    /// <see cref="Document"/>s.
+    /// </summary>
+    public abstract class DocumentEmitter
+    {
+        /// <summary>
+        /// Raised when a new document has been detected in the underlying
+        /// event stream.
+        /// </summary>
+        public event EventHandler<NewDocumentEventArgs> NewDocument;
+
+        /// <summary>
+        /// Signals the emitter will not be sending any further events.
+        /// </summary>
+        internal event EventHandler EmitterCompleted;
+
+        /// <summary>
+        /// Emits a <see cref="NewDocument"/> event, provided
+        /// <see cref="EnableRaisingEvents"/> is <c>true</c>. Use this from
+        /// derived classes rather than invoking <see cref="NewDocument"/>
+        /// directly.
+        /// </summary>
+        /// <param name="newDocument">The document to emit to listeners.</param>
+        protected void EmitDocument(Document newDocument)
+        {
+            if (EnableRaisingEvents)
+            {
+                NewDocument?.Invoke(this, new NewDocumentEventArgs(newDocument));
+            }
+        }
+
+        /// <summary>
+        /// Emits a <see cref="Completed"/> event, indicating that no further
+        /// events will be coming from this <see cref="DocumentEmitter"/>.
+        /// </summary>
+        protected void Completed()
+        {
+            EmitterCompleted?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// Enable or disable the raising of <see cref="NewDocument"/> events.
+        /// Defaults to <c>true</c>. This should be used to temporarily disable
+        /// event emission without also signalling completion.
+        /// </summary>
+        public virtual bool EnableRaisingEvents { get; set; } = true;
+
+        /// <summary>
+        /// The process for emitting events.
+        /// </summary>
+        /// <remarks>
+        /// This method will be run in a task on the task pool. Returning from
+        /// this method indicates nothing to the <see cref="Pipeline"/> about the
+        /// completion of the source, unless <see cref="Completed"/> is called
+        /// first. As such, it can be used to implement long-running tasks (such
+        /// as polling), or set-up tasks (for configuring an underlying event source).
+        /// In the case of a long-running task, like a polling activity, you should
+        /// check whether the <paramref name="token"/> has been cancelled, call
+        /// <see cref="Completed"/> and return from this method.
+        /// </remarks>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        protected abstract Task EmitDocuments(CancellationToken token);
+
+        /// <summary>
+        /// Start emitting objects from the underlying event stream, with no
+        /// cancellation possible.
+        /// </summary>
+        public void Start()
+        {
+            Start(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Start emitting objects from the underlying event stream, continuing
+        /// until the <paramref name="token"/> is cancelled.
+        /// </summary>
+        /// <param name="token">A cancellation token that can be used by other
+        /// objects or threads to receive notice of cancellation.</param>
+        public void Start(CancellationToken token)
+        {
+            EnableRaisingEvents = true;
+            Task.Run(() => { EmitDocuments(token).ConfigureAwait(false); }, token);
+        }
+    }
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Event arguments for the <see cref="DocumentEmitter.NewDocument"/> event.
+    /// </summary>
+    public class NewDocumentEventArgs : EventArgs
+    {
+        public NewDocumentEventArgs(Document document)
+        {
+            Document = document;
+        }
+
+        /// <summary>
+        /// Gets the document that was emitted.
+        /// </summary>
+        public Document Document { get; }
+    }
+}
---
+++ src/Waives.Reactive/EventingDocumentSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace Waives.Reactive
+{
+    /// <summary>
+    /// Adapter class converting an event stream from a <see cref="DocumentEmitter"/>
+    /// into a <see cref="DocumentSource"/> for use as the starting point of a
+    /// <see cref="Pipeline"/>.
+    /// </summary>
+    public sealed class EventingDocumentSource : DocumentSource, IDisposable
+    {
+        private readonly IDisposable _connection;
+
+        private EventingDocumentSource(IObservable<Document> buffer, IDisposable connection) : base(buffer)
+        {
+            _connection = connection;
+        }
+
+        /// <summary>
+        /// Create an <see cref="EventingDocumentSource" /> from the events raised by
+        /// the given <see cref="DocumentEmitter"/>. Events raised will be cached from
+        /// the point at which this method is called to ensure the pipeline does not
+        /// miss any documents.
+        /// </summary>
+        /// <param name="emitter">The source of new document events.</param>
+        /// <param name="token">A token for cancelling the <paramref name="emitter"/>'s work.</param>
+        /// <returns>A new <see cref="EventingDocumentSource"/> configured to capture new document
+        /// events from the <paramref name="emitter"/>.</returns>
+        public static EventingDocumentSource Create(DocumentEmitter emitter, CancellationToken token)
+        {
+            var documents = Observable
+                .FromEventPattern<NewDocumentEventArgs>(emitter, nameof(emitter.NewDocument))
+                .Select(e => e.EventArgs.Document)
+                .Replay();
+
+            var documentSource = new EventingDocumentSource(documents, documents.Connect());
+            emitter.EmitterCompleted += (s, e) => { documentSource.Dispose(); };
+            emitter.Start(token);
+
+            return documentSource;
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
