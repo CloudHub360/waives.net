26c25ef99db4d49edb2714a8b0cef25d84780dc7
Alastair Smith <github@alastairsmith.me.uk>
2018-08-21 10:38:07 +0100

    Improve documentation on Document
    Add implementation notes to `OpenStream()`, and provide more clarity on
    the purpose of `SourceId`.

--- src/Waives.Reactive/Document.cs
+++ src/Waives.Reactive/Document.cs
@@ -8,6 +8,11 @@ namespace Waives.Reactive
     /// </summary>
     public abstract class Document
     {
+        /// <summary>
+        /// Initialises the <see cref="Document"/> with the given <paramref name="sourceId"/>.
+        /// </summary>
+        /// <param name="sourceId">A value uniquely identifying this doucment.</param>
+        /// <seealso cref="SourceId"/>
         protected Document(string sourceId)
         {
             if (string.IsNullOrWhiteSpace(sourceId))
@@ -19,16 +24,39 @@ namespace Waives.Reactive
         }
 
         /// <summary>
-        /// E.g. File path, database row ID, etc.
+        /// Gets an identifier for the source of the <see cref="Document"/>.
         /// </summary>
+        /// <remarks>
+        /// How the SourceId is defined is up to applications to decide. It is not used
+        /// internally by the Waives SDK, and is provided for the convenience of consuming
+        /// applications. Example values for this property might be path to the document's
+        /// file on disk, a database row ID, a URI to fetch the document from, etc.
+        /// </remarks>
         public string SourceId { get; }
 
         /// <summary>
-        ///
+        /// Opens a <see cref="Stream"/> onto the underlying Document resource (e.g. file on
+        /// disk, blob, etc.).
         /// </summary>
-        /// <returns></returns>
+        /// <remarks>
+        /// <para>
+        /// This method is called by the Waives SDK to create a document resource in Waives.
+        /// The SDK expects to be able to use the <see cref="Stream"/> as returned by this
+        /// method, and will dispose of it when the document has been created. This is to
+        /// ensure the <see cref="Stream"/> is kept open for the shortest possible time.
+        /// As such, this method should be implemented to return a new <see cref="Stream"/>
+        /// on each call.
+        /// </para>
+        /// <para>
+        /// If you need the document contents for your application's purposes too and the
+        /// stream cannot be easily re-established, copy it to a <see cref="MemoryStream"/>
+        /// and pass that to your implementation of <see cref="Document"/>.
+        /// </para>
+        /// </remarks>
+        /// <returns>A new <see cref="Stream"/> ready to be opened for reading.</returns>
         public abstract Task<Stream> OpenStream();
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
         {
             return Equals(obj as Document);
@@ -54,11 +82,13 @@ namespace Waives.Reactive
             return string.Equals(x.SourceId, y.SourceId);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
             return SourceId.GetHashCode();
         }
 
+        /// <inheritdoc />
         public override string ToString()
         {
             return SourceId;
