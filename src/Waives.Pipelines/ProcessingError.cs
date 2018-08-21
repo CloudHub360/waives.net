be7441419d12541e8f5676b523c811f215862a8c
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-21 14:30:36 +0100

    Add comments to types

--- src/Waives.Pipelines/ProcessingError.cs
+++ src/Waives.Pipelines/ProcessingError.cs
@@ -2,6 +2,12 @@
 
 namespace Waives.Pipelines
 {
+    /// <summary>
+    /// Represents an error that occurred during processing, either on
+    /// a WaivesDocument (during processing) or a Document (during creation).
+    /// </summary>
+    /// <typeparam name="T">The type of document on which the error occurred,
+    /// either <see cref="WaivesDocument"/> or <see cref="Document"/></typeparam>
     internal class ProcessingError<T>
     {
         public T Document { get; }
