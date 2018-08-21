d8eb7a2745a48e8322ee2e1d6801a67383c15370
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-21 14:30:36 +0100

    Add comments to types

--- src/Waives.Pipelines/DocumentError.cs
+++ src/Waives.Pipelines/DocumentError.cs
@@ -2,9 +2,19 @@
 
 namespace Waives.Pipelines
 {
+    /// <summary>
+    /// Represents an error that has occurred during processing of a
+    /// <see cref="Document"/> through a <see cref="Pipeline"/>
+    /// </summary>
     public class DocumentError
     {
+        /// <summary>
+        /// The <see cref="Document"/> on which the error occurred
+        /// </summary>
         public Document Document { get; }
+        /// <summary>
+        /// The exception containing details of the error
+        /// </summary>
         public Exception Exception { get; }
 
         public DocumentError(Document document, Exception exception)
