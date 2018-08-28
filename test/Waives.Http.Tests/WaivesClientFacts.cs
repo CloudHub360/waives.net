3f1008b397df4da4e28e5a6d5a4d0ab97aea1bf2
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-28 15:46:32 +0100

    Remove unused using statements

--- test/Waives.Http.Tests/WaivesClientFacts.cs
+++ test/Waives.Http.Tests/WaivesClientFacts.cs
@@ -1,8 +1,6 @@
 ﻿using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Waives.Http.Logging;
 using Xunit;
