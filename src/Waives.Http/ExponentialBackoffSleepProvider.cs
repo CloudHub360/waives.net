e349fb67d40959c73874c5ce6081a04ce9738a08
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-29 14:17:47 +0100

    Add ExponentialBackoffSleepProvider

---
+++ src/Waives.Http/ExponentialBackoffSleepProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Waives.Http
+{
+    public class ExponentialBackoffSleepProvider
+    {
+        private readonly Random _jitterer = new Random();
+
+        // With 8 retries, we get retries at the following base times:
+        // 0s, 1s, 3s, 7s, 15s, 31s, 63s and 127s
+        // But we will also have additional jitter time of between
+        // 0 and 36s (1+2+..+8s)
+        public TimeSpan GetSleepDuration(int retry)
+        {
+            var baseDelay = TimeSpan.FromSeconds(Math.Pow(2, retry - 1));
+
+            // Add some jitter so we spread out retried requests if we had
+            // a system glitch that affected multiple requests
+            var jitterDelay = TimeSpan.FromMilliseconds(
+                _jitterer.Next(0, 1000 * retry));
+
+            return baseDelay + jitterDelay;
+        }
+    }
+}
---
+++ test/Waives.Http.Tests/ExponentialBackoffSleepProviderFacts.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Waives.Http.Tests
+{
+    public class ExponentialBackoffSleepProviderFacts
+    {
+        [Theory]
+        [InlineData(1, 1000, 2000)] //1000 + 1*1000
+        [InlineData(2, 2000, 4000)] //2000 + 2*1000
+        [InlineData(3, 4000, 7000)] //4000 + 3*1000
+        [InlineData(4, 8000, 12000)] //8000 + 4*1000
+        [InlineData(5, 16000, 21000)] //16000 + 5*1000
+        [InlineData(6, 32000, 38000)] //32000 + 6*1000
+        [InlineData(7, 64000, 71000)] //64000 + 7*1000
+        [InlineData(8, 128000, 136000)] //128000 + 8*1000
+        public void Test(int retry, int expectedMinDuration, int expectedMaxDuration)
+        {
+            var sut = new ExponentialBackoffSleepProvider();
+            var timespan = sut.GetSleepDuration(retry);
+
+            Assert.InRange(timespan.TotalMilliseconds, expectedMinDuration, expectedMaxDuration);
+        }
+    }
+}
