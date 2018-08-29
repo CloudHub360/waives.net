72c6c6850865cc92931b2675790542221ac09fe1
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-29 20:06:01 +0100

    Add HttpRequestMessageTemplate
    This duplicates the essential properties that we use on an HttpRequestMessage.

---
+++ src/Waives.Http/HttpRequestMessageTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Waives.Http
+{
+    public class HttpRequestMessageTemplate
+    {
+        public HttpRequestMessageTemplate(HttpMethod method, Uri requestUri)
+        {
+            Method = method ?? throw new ArgumentNullException(nameof(method));
+            RequestUri = requestUri ?? throw new ArgumentNullException(nameof(requestUri));
+            Headers = new List<KeyValuePair<string, string>>();
+        }
+
+        public HttpMethod Method { get; set; }
+        public Uri RequestUri { get; set; }
+        public HttpContent Content { get; set; }
+        public List<KeyValuePair<string, string>> Headers { get; }
+    }
+}
