faab1c677b3857799710cce5a84504c2391aeea3
Mark Wightman <mark.wightman@cloudhub360.com>
2018-08-29 17:04:25 +0100

    Don't grow jitter with each retry
    We decided this was overkill

--- src/Waives.Http/ExponentialBackoffSleepProvider.cs
+++ src/Waives.Http/ExponentialBackoffSleepProvider.cs
@@ -8,8 +8,6 @@ namespace Waives.Http
 
         // With 8 retries, we get retries at the following base times:
         // 0s, 1s, 3s, 7s, 15s, 31s, 63s and 127s
-        // But we will also have additional jitter time of between
-        // 0 and 36s (1+2+..+8s)
         public TimeSpan GetSleepDuration(int retry)
         {
             var baseDelay = TimeSpan.FromSeconds(Math.Pow(2, retry - 1));
@@ -17,7 +15,7 @@ namespace Waives.Http
             // Add some jitter so we spread out retried requests if we had
             // a system glitch that affected multiple requests
             var jitterDelay = TimeSpan.FromMilliseconds(
-                _jitterer.Next(0, 1000 * retry));
+                _jitterer.Next(0, 1000));
 
             return baseDelay + jitterDelay;
         }
