53f49eaf875713ee920be21289ab023c15a476b1
Alastair Smith <github@alastairsmith.me.uk>
2018-09-03 11:29:13 +0100

    Handle failed requests in new Decorator
    A new `FailedRequestHandlingRequestSender` decorator class will catch
    the failed requests _after_ the retries from Polly in the
    ReliableRequestSender, and the ones which cannot be retried. These will
    then be thrown as `WaivesApiException`s.

---
+++ src/Waives.Http/FailedRequestHandlingRequestSender.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Waives.Http.Responses;
+
+namespace Waives.Http
+{
+    internal class FailedRequestHandlingRequestSender : IHttpRequestSender
+    {
+        private readonly IHttpRequestSender _wrappedRequestSender;
+
+        public FailedRequestHandlingRequestSender(IHttpRequestSender underlyingRequestSender)
+        {
+            _wrappedRequestSender = underlyingRequestSender;
+        }
+
+        public int Timeout
+        {
+            get => _wrappedRequestSender.Timeout;
+            set => _wrappedRequestSender.Timeout = value;
+        }
+
+        public void Authenticate(string accessToken)
+        {
+            _wrappedRequestSender.Authenticate(accessToken);
+        }
+
+        public async Task<HttpResponseMessage> Send(HttpRequestMessageTemplate request)
+        {
+            var response = await _wrappedRequestSender.Send(request).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            var responseContentType = response.Content.Headers.ContentType.MediaType;
+            if (responseContentType == "application/json")
+            {
+                var error = await response.Content.ReadAsAsync<Error>().ConfigureAwait(false);
+                throw new WaivesApiException(error.Message);
+            }
+
+            throw new WaivesApiException($"Unknown Waives error occured: {(int)response.StatusCode} {response.ReasonPhrase}");
+        }
+    }
+}
