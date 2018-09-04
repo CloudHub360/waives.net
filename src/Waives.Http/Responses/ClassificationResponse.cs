dc9fc16dd4b987849f9b6b6c234a7188a90990b5
Alastair Smith <github@alastairsmith.me.uk>
2018-09-04 14:14:23 +0100

    Document ClassificationResponse

--- src/Waives.Http/Responses/ClassificationResponse.cs
+++ src/Waives.Http/Responses/ClassificationResponse.cs
@@ -12,26 +12,67 @@ namespace Waives.Http.Responses
         internal ClassificationResult ClassificationResults { get; set; }
     }
 
+    /// <summary>
+    /// Represents the results of a <see cref="Document.Classify"/> operation.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// In most cases, the <see cref="DocumentType"/> and <see cref="IsConfident"/> flags are the only ones you should use
+    /// in production.
+    /// </para>
+    /// <para>
+    /// If <see cref="IsConfident"/> is false, you should not trust <see cref="DocumentType"/>. Depending on your
+    /// scenario, you may want to route this document for a person to review, or handle differently in some other way.
+    /// </para>
+    /// <para>In general, unconfident classifications indicate that the content of the document is substantially different
+    /// from any documents contained in your sample set.Therefore you may also wish to capture these documents, analyse them
+    /// and if appropriate add some to your samples and retrain your classifier.
+    /// </para>
+    /// </remarks>
     public class ClassificationResult
     {
+        /// <summary>
+        /// Gets the document type that Waives believes the document to be.
+        /// </summary>
         [JsonProperty("document_type")]
         public string DocumentType { get; set; }
 
+        /// <summary>
+        /// Gets a value giving an indication of how confident Waives is that the
+        /// document type is correct.
+        /// </summary>
         [JsonProperty("relative_confidence")]
         public decimal RelativeConfidence { get; set; }
 
+        /// <summary>
+        /// Gets a value indicating whether Waives is confident that document type
+        /// is correct. If the value is <c>false</c>, you should not trust the
+        /// value of <see cref="DocumentType"/>.
+        /// </summary>
         [JsonProperty("is_confident")]
         public bool IsConfident { get; set; }
 
+        /// <summary>
+        /// Gets a collection of scores for each possible document type in the classifer.
+        /// </summary>
         [JsonProperty("document_type_scores")]
         public IEnumerable<DocumentTypeScore> DocumentTypeScores { get; set; }
     }
 
+    /// <summary>
+    /// Represents a classification score for a given document type.
+    /// </summary>
     public class DocumentTypeScore
     {
+        /// <summary>
+        /// Gets the document type associated with this score.
+        /// </summary>
         [JsonProperty("document_type")]
         public string DocumentType { get; set; }
 
+        /// <summary>
+        /// Gets the score associated with this document type.
+        /// </summary>
         [JsonProperty("score")]
         public decimal Score { get; set; }
     }
