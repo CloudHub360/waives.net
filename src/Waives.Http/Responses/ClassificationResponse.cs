4147d3ad387229ff9d634b51f7472b2e5e0d3a16
Alastair Smith <github@alastairsmith.me.uk>
2018-09-04 14:14:32 +0100

    Make result fields internally mutable only

--- src/Waives.Http/Responses/ClassificationResponse.cs
+++ src/Waives.Http/Responses/ClassificationResponse.cs
@@ -35,14 +35,14 @@ namespace Waives.Http.Responses
         /// Gets the document type that Waives believes the document to be.
         /// </summary>
         [JsonProperty("document_type")]
-        public string DocumentType { get; set; }
+        public string DocumentType { get; internal set; }
 
         /// <summary>
         /// Gets a value giving an indication of how confident Waives is that the
         /// document type is correct.
         /// </summary>
         [JsonProperty("relative_confidence")]
-        public decimal RelativeConfidence { get; set; }
+        public decimal RelativeConfidence { get; internal set; }
 
         /// <summary>
         /// Gets a value indicating whether Waives is confident that document type
@@ -50,13 +50,13 @@ namespace Waives.Http.Responses
         /// value of <see cref="DocumentType"/>.
         /// </summary>
         [JsonProperty("is_confident")]
-        public bool IsConfident { get; set; }
+        public bool IsConfident { get; internal set; }
 
         /// <summary>
         /// Gets a collection of scores for each possible document type in the classifer.
         /// </summary>
         [JsonProperty("document_type_scores")]
-        public IEnumerable<DocumentTypeScore> DocumentTypeScores { get; set; }
+        public IEnumerable<DocumentTypeScore> DocumentTypeScores { get; internal set; }
     }
 
     /// <summary>
@@ -68,12 +68,12 @@ namespace Waives.Http.Responses
         /// Gets the document type associated with this score.
         /// </summary>
         [JsonProperty("document_type")]
-        public string DocumentType { get; set; }
+        public string DocumentType { get; internal set; }
 
         /// <summary>
         /// Gets the score associated with this document type.
         /// </summary>
         [JsonProperty("score")]
-        public decimal Score { get; set; }
+        public decimal Score { get; internal set; }
     }
 }
