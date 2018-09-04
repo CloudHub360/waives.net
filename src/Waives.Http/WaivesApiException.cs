fea2aed6cd1c9df361365e2b7256e0626a378d5c
Alastair Smith <github@alastairsmith.me.uk>
2018-09-04 12:21:06 +0100

    Add documentation to WaivesApiException

--- src/Waives.Http/WaivesApiException.cs
+++ src/Waives.Http/WaivesApiException.cs
@@ -2,16 +2,23 @@ using System;
 
 namespace Waives.Http
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents an error occurring in calling the Waives platform API
+    /// </summary>
     public class WaivesApiException : Exception
     {
+        /// <inheritdoc />
         public WaivesApiException()
         {
         }
 
+        /// <inheritdoc />
         public WaivesApiException(string message) : base(message)
         {
         }
 
+        /// <inheritdoc />
         public WaivesApiException(string message, Exception innerException) : base(message, innerException)
         {
         }
