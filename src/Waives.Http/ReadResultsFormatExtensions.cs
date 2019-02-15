72e23c1d69864553444aba8c70064715d0e16e20
Mark Wightman <mark.wightman@cloudhub360.com>
2019-02-15 15:13:26 +0000

    Move enum to string converter to an extension method

---
+++ src/Waives.Http/ReadResultsFormatExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Waives.Http
+{
+    public static class ReadResultsFormatExtensions
+    {
+        public static string ToMimeType(this ReadResultsFormat format)
+        {
+            switch (format)
+            {
+                case ReadResultsFormat.Text:
+                    return "text/plain";
+                case ReadResultsFormat.Pdf:
+                    return "application/pdf";
+                default:
+                    return "application/vnd.waives.resultformats.read+zip";
+            }
+        }
+    }
+}
