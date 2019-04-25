bdd2a1284a5f6599478b161ac53061a3b476a595
Mark Wightman <mark.wightman@cloudhub360.com>
2019-04-25 13:40:27 +0100

    Add Create Document from Uri method to HTTP Client

---
+++ src/Waives.Http/Requests/ImportDocumentRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Waives.Http.Requests
+{
+    public class ImportDocumentRequest
+    {
+        [JsonProperty("url")]
+#pragma warning disable CA1056 // Uri properties should not be strings
+        public string Url { get; set; }
+#pragma warning restore CA1056 // Uri properties should not be strings
+    }
+}
