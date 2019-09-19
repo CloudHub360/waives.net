42ceff04aebbba2fea79d79605acda6d39311943
Alastair Smith <github@alastairsmith.me.uk>
2019-09-19 21:41:29 +0100

    Move access token tests to new fixtures
    The old Login() tests are now applicable to the AccessTokenService
    class. New tests for the TokenFetchingRequestSender have been added to
    verify the cache is correctly used. It turned out the cache was being
    ignored because no Context was supplied, and the default cache key
    strategy uses the Context's OperationKey value. This has been rectified
    here too.

---
+++ test/Waives.Http.Tests/RequestHandling/TokenFetchingRequestSenderFacts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NSubstitute;
+using Waives.Http.RequestHandling;
+using Xunit;
+
+namespace Waives.Http.Tests.RequestHandling
+{
+    public class TokenFetchingRequestSenderFacts
+    {
+        private readonly IHttpRequestSender _requestSender = Substitute.For<IHttpRequestSender>();
+        private readonly TokenFetchingRequestSender _sut;
+
+        public TokenFetchingRequestSenderFacts()
+        {
+            _sut = new TokenFetchingRequestSender(
+                new AccessTokenService("clientId", "clientSecret", _requestSender),
+                _requestSender);
+
+            _requestSender
+                .Send(Arg.Is<HttpRequestMessageTemplate>(
+                    r => !r.Headers.ContainsKey("Authorization")))
+                .Returns(ci => Response.GetToken(ci.Arg<HttpRequestMessageTemplate>()));
+        }
+
+        [Fact]
+        public async Task Send_retrieves_an_access_token_for_the_request()
+        {
+            await _sut.Send(new HttpRequestMessageTemplate(HttpMethod.Get, new Uri("/documents", UriKind.Relative)));
+            await _requestSender
+                .Received(1)
+                .Send(Arg.Is<HttpRequestMessageTemplate>(
+                    r => r.RequestUri == new Uri("/oauth/token", UriKind.Relative)));
+        }
+
+        [Fact]
+        public async Task Send_retrieves_an_access_token_only_once_while_the_token_is_valid()
+        {
+            await _sut.Send(new HttpRequestMessageTemplate(HttpMethod.Get, new Uri("/documents", UriKind.Relative)));
+            await _sut.Send(new HttpRequestMessageTemplate(HttpMethod.Get, new Uri("/documents", UriKind.Relative)));
+            await _sut.Send(new HttpRequestMessageTemplate(HttpMethod.Get, new Uri("/documents", UriKind.Relative)));
+
+            await _requestSender
+                .Received(1)
+                .Send(Arg.Is<HttpRequestMessageTemplate>(
+                    r => r.RequestUri == new Uri("/oauth/token", UriKind.Relative)));
+        }
+
+        [Fact]
+        public async Task Send_adds_the_access_token_to_the_onward_request()
+        {
+            var template = new HttpRequestMessageTemplate(HttpMethod.Get, new Uri("/documents", UriKind.Relative));
+
+            await _sut.Send(template);
+
+            await _requestSender
+                .Received(1)
+                .Send(Arg.Is<HttpRequestMessageTemplate>(
+                    r => r.RequestUri == template.RequestUri &&
+                         r.Headers.ContainsKey("Authorization")));
+        }
+    }
+}
