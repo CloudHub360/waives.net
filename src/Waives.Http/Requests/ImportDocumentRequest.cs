50240dc3a228bd34cac3b35fe4eb433224de08f7
Alastair Smith <github@alastairsmith.me.uk>
2019-10-01 12:30:34 +0100

    Convert ImportDocumentRequest Url from string

--- src/Waives.Http/Requests/ImportDocumentRequest.cs
+++ src/Waives.Http/Requests/ImportDocumentRequest.cs
@@ -1,12 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace Waives.Http.Requests
 {
     public class ImportDocumentRequest
     {
         [JsonProperty("url")]
-#pragma warning disable CA1056 // Uri properties should not be strings
-        public string Url { get; set; }
-#pragma warning restore CA1056 // Uri properties should not be strings
+        public Uri Url { get; set; }
     }
 }
