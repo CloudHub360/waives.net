de7e1094c70ca68101524700a4414a709b6a9397
Alastair Smith <github@alastairsmith.me.uk>
2019-10-01 18:04:32 +0100

    Rename doc to document: DocumentProcessor

--- src/Waives.Pipelines/DocumentProcessor.cs
+++ src/Waives.Pipelines/DocumentProcessor.cs
@@ -7,19 +7,19 @@ namespace Waives.Pipelines
 {
     internal class DocumentProcessor : IDocumentProcessor
     {
-        private readonly Func<Document, CancellationToken, Task<WaivesDocument>> _docCreator;
-        private readonly IEnumerable<Func<WaivesDocument, CancellationToken, Task<WaivesDocument>>> _docActions;
-        private readonly Func<WaivesDocument, Task> _docDeleter;
+        private readonly Func<Document, CancellationToken, Task<WaivesDocument>> _documentCreator;
+        private readonly IEnumerable<Func<WaivesDocument, CancellationToken, Task<WaivesDocument>>> _documentActions;
+        private readonly Func<WaivesDocument, Task> _documentDeleter;
         private readonly Action<Exception, Document> _onDocumentException;
 
-        public DocumentProcessor(Func<Document, CancellationToken, Task<WaivesDocument>> docCreator,
-            IEnumerable<Func<WaivesDocument, CancellationToken, Task<WaivesDocument>>> docActions,
-            Func<WaivesDocument, Task> docDeleter,
+        public DocumentProcessor(Func<Document, CancellationToken, Task<WaivesDocument>> documentCreator,
+            IEnumerable<Func<WaivesDocument, CancellationToken, Task<WaivesDocument>>> documentActions,
+            Func<WaivesDocument, Task> documentDeleter,
             Action<Exception, Document> onDocumentException)
         {
-            _docCreator = docCreator ?? throw new ArgumentNullException(nameof(docCreator));
-            _docActions = docActions ?? throw new ArgumentNullException(nameof(docActions));
-            _docDeleter = docDeleter ?? throw new ArgumentNullException(nameof(docDeleter));
+            _documentCreator = documentCreator ?? throw new ArgumentNullException(nameof(documentCreator));
+            _documentActions = documentActions ?? throw new ArgumentNullException(nameof(documentActions));
+            _documentDeleter = documentDeleter ?? throw new ArgumentNullException(nameof(documentDeleter));
             _onDocumentException = onDocumentException ?? throw new ArgumentNullException(nameof(onDocumentException));
         }
 
@@ -28,7 +28,7 @@ namespace Waives.Pipelines
             WaivesDocument waivesDocument = null;
             try
             {
-                waivesDocument = await _docCreator(document, cancellationToken);
+                waivesDocument = await _documentCreator(document, cancellationToken);
                 await RunActionsAsync(waivesDocument, cancellationToken).ConfigureAwait(false);
             }
             catch (Exception e)
@@ -39,16 +39,16 @@ namespace Waives.Pipelines
             {
                 if (waivesDocument != null)
                 {
-                    await _docDeleter(waivesDocument);
+                    await _documentDeleter(waivesDocument);
                 }
             }
         }
 
         private async Task RunActionsAsync(WaivesDocument document, CancellationToken cancellationToken = default)
         {
-            foreach (var docAction in _docActions)
+            foreach (var documentAction in _documentActions)
             {
-                document = await docAction(document, cancellationToken).ConfigureAwait(false);
+                document = await documentAction(document, cancellationToken).ConfigureAwait(false);
             }
         }
     }
