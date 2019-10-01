526cedc9f7a35548606e459831b1a85555ed46a3
Alastair Smith <github@alastairsmith.me.uk>
2019-10-01 10:10:32 +0100

    Resolve R# issues in WaivesOptions
    Ignore suggestions to make setters private; make default concurrency
    constant private.

--- src/Waives.Pipelines/WaivesOptions.cs
+++ src/Waives.Pipelines/WaivesOptions.cs
@@ -12,17 +12,20 @@ namespace Waives.Pipelines
         /// <summary>
         /// The Waives API Client ID to use when authenticating with the service.
         /// </summary>
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public string ClientId { get; set; }
 
         /// <summary>
         /// The Waives API Client Secret to use when authenticating with the service.
         /// </summary>
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public string ClientSecret { get; set; }
 
         /// <summary>
         /// The URL on which the Waives API is running. This only needs overriding for
         /// connections to instances other than the public cloud-hosted service.
         /// </summary>
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public Uri ApiUrl { get; set; } = new Uri(WaivesClient.DefaultUrl);
 
         /// <summary>
@@ -30,13 +33,15 @@ namespace Waives.Pipelines
         /// in existence in the Waives account; if set to <c>false</c>, no such clean up will be completed.
         /// Defaults to <c>true</c>.
         /// </summary>
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public bool DeleteExistingDocuments { get; set; } = true;
 
         /// <summary>
         /// The maximum number of documents to process concurrently.
         /// </summary>
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public int MaxConcurrency { get; set; } = DefaultMaximumConcurrentDocuments;
 
-        public const byte DefaultMaximumConcurrentDocuments = 10;
+        private const byte DefaultMaximumConcurrentDocuments = 10;
     }
 }
