af160c234b7a7d25cb5640ccd7f4b931eb0a3529
Alastair Smith <github@alastairsmith.me.uk>
2019-10-01 09:56:56 +0100

    Resolve R# issues in WorkPoolFacts
    Dispose of the WorkPool after the tests have run, use a local function
    instead of a lambda variable.

--- test/Waives.Pipelines.Tests/WorkPoolFacts.cs
+++ test/Waives.Pipelines.Tests/WorkPoolFacts.cs
@@ -5,20 +5,22 @@ using Xunit;
 
 namespace Waives.Pipelines.Tests
 {
-    public class WorkPoolFacts
+    public class WorkPoolFacts : IDisposable
     {
+        private WorkPool _sut;
+
         [Fact]
         public async Task Post_runs_supplied_func()
         {
-            var sut = new WorkPool(3);
+            _sut = new WorkPool(3);
             var hasRun = false;
 
-            sut.Post(() =>
+            _sut.Post(() =>
             {
                 hasRun = true;
                 return Task.CompletedTask;
             });
-            await sut.WaitAsync();
+            await _sut.WaitAsync();
 
             Assert.True(hasRun);
         }
@@ -30,27 +32,35 @@ namespace Waives.Pipelines.Tests
         [InlineData(1000,1001)]
         public async Task Post_runs_to_concurrency_limit(int concurrencyLimit, int postCount)
         {
-            var sut = new WorkPool(concurrencyLimit);
+            _sut = new WorkPool(concurrencyLimit);
             var currentConcurrency = 0;
             var maxObservedConcurrency = 0;
 
-            Func<Task> func = async () =>
+            async Task SimulatedWork()
             {
                 Interlocked.Increment(ref currentConcurrency);
                 await Task.Delay(10); // simulate work
-                lock (sut)
+
+                lock (_sut)
                 {
                     maxObservedConcurrency = Math.Max(maxObservedConcurrency, currentConcurrency);
                 }
+
                 Interlocked.Decrement(ref currentConcurrency);
-            };
-            for (int i = 0; i < postCount; i++)
-            {
-                sut.Post(func);
             }
-            await sut.WaitAsync();
+
+            for (var i = 0; i < postCount; i++)
+            {
+                _sut.Post(SimulatedWork);
+            }
+            await _sut.WaitAsync();
 
             Assert.Equal(concurrencyLimit, maxObservedConcurrency);
+        }
+
+        public void Dispose()
+        {
+            _sut?.Dispose();
         }
     }
 }
